7d981b1eaf1dde75b6a9c84920c2f8eab2ce924b
hieu@buuuk.com <hieu.tran21198@gmail.com>
2023-09-13 12:27:28 +0700

    feat(record):added record features

--- record/record.go
+++ record/record.go
@@ -1,14 +1,37 @@
 package record
 
+// FindKeysByValue returns all keys that have a given value
 func FindKeysByValue[K, V comparable](m map[K]V, values ...V) []K {
 	result := make([]K, 0)
 
 	for _, v := range values {
-		for k, _ := range m {
+		for k := range m {
 			if v == m[k] {
 				result = append(result, k)
 			}
 		}
+	}
+
+	return result
+}
+
+// Keys returns all keys.
+func Keys[K comparable, V any](m map[K]V) []K {
+	result := make([]K, 0)
+
+	for k := range m {
+		result = append(result, k)
+	}
+
+	return result
+}
+
+// Vals returns all values.
+func Vals[K comparable, V any](m map[K]V) []V {
+	result := make([]V, 0)
+
+	for _, v := range m {
+		result = append(result, v)
 	}
 
 	return result
